4f4c3e480e7b0c1126b19ce63a50623352da8620
Made-of-Clay <adam.m.leis@gmail.com>
2016-05-31 17:07:13 -0400

    working through filter bugs for calc

--- app/components/mvl-calc/button-val.filter.js
+++ app/components/mvl-calc/button-val.filter.js
@@ -5,13 +5,24 @@
         .module('marvel')
         .filter('buttonVal', buttonVal);
 
-    buttonVal.$inject = ['_'];
-    function buttonVal(_) {
+    buttonVal.$inject = ['$sce'];
+    function buttonVal($sce) {
         return buttonValFilter;
 
         function buttonValFilter(value) {
-            if(value)
-            return value;
+            var val;
+            if(isNaN(value)) {
+                // backspace or clear
+                if(value.toLowerCase() === 'backspace') {
+                    val = $sce.trustAsHtml('&laquo;');
+                } else if(value.toLowerCase() === 'clear') {
+                    val = 'C';
+                } else console.error('Whaaa?');
+            } else {
+                val = value;
+            }
+
+            return val;
         }
     }
 })();
--- app/components/mvl-calc/mvl-calc.controller.js
+++ app/components/mvl-calc/mvl-calc.controller.js
@@ -13,13 +13,16 @@
         vm.title = 'MvlCalcController';
         vm.buttons = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 'backspace', 0, 'clear' ];
 
+        vm.checkVal = checkVal;
+
         activate();
         ////////////////
         function activate() {
             console.log('activated');
         }
-        function checkVal(value) {
+        function checkVal(value, i) {
             console.log('value',value);
+            console.log('i',i);
             return value;
         }
     }
