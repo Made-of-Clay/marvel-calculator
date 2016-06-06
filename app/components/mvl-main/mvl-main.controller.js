e142cb26bb2e59eeb8e895b3da8cb229cfc8c6ba
Made-of-Clay <adam.m.leis@gmail.com>
2016-06-06 09:01:56 -0400

    add logic for figuring out color

--- app/components/mvl-main/mvl-main.controller.js
+++ app/components/mvl-main/mvl-main.controller.js
@@ -24,14 +24,31 @@
             $rootScope.$on('change.rank', function rankChanged(event, newRank) {
                 vm.rank = newRank;
                 displayNewRank(newRank);
+                updateColor();
             });
             $rootScope.$on('change.result', function resultChanged(event, newResult) {
                 vm.result = newResult;
+                updateColor();
             });
         }
         function displayNewRank(rank) {
             var newDisp = rank.abbv + ' (' + rank.mid + ')';
             $rootScope.$broadcast('change.display', newDisp);
+        }
+        function updateColor() {
+            if(_.isNull(vm.rank)) { console.error('noop'); return; }
+            var colorMatch;
+            var ignore = false;
+            _.forEach(['white','green','yellow','red'], function checkEachColor(color) {
+                if(ignore) return;
+                var min = vm.rank[color][0];
+                var max = vm.rank[color][0];
+                if(_.inRange(vm.result, min, max+1)) { // max should be inclusive
+                    colorMatch = color;
+                    ignore = true; // skips logic for future iterations
+                }
+            });
+            $rootScope.$broadcast('change.color', colorMatch);
         }
     }
 })();
