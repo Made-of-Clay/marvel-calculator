4004e3c817b10b976d777973aa61e8407631b8be
Made-of-Clay <adam.m.leis@gmail.com>
2018-04-24 22:04:33 -0400

    add colors & general styles to app

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,14 @@
                     :progress-width="dims.progressWidth"
                     :nob-radius="dims.nobRadius"
                     :min="0"
-                    :max="rankCount"></circle-slider>
-                <span class="power-rank-label">
+                    :max="rankCount"
+                    :circle-color="circleColor"
+                    :progress-color="color.progress"
+                    :knob-color="color.knob"
+                    class="circle-slider-graphic"></circle-slider>
+                <span class="power-rank-label circle-slider-label">
                     <span class="power-rank-key">{{powerRankKey}}</span>
-                    <span class="power-rank-mid">{{powerRankMid}}</span>
+                    <span v-show="showMid" class="power-rank-mid">{{powerRankMid}}</span>
                 </span>
             </div>
             <div class="circle-slider-container roll-result">
@@ -22,8 +26,12 @@
                     :progress-width="dims.progressWidth"
                     :nob-radius="dims.nobRadius"
                     :min="1"
-                    :max="101"></circle-slider>
-                <span class="roll-result-label">{{rollResult}}</span>
+                    :max="100"
+                    :circle-color="circleColor"
+                    :progress-color="color.progress"
+                    :knob-color="color.knob"
+                    class="circle-slider-graphic"></circle-slider>
+                <span class="roll-result-label circle-slider-label">{{rollResult}}</span>
             </div>
         </div>
     </main>
@@ -33,6 +41,12 @@
 import powerRanks from '../power-ranks';
 
 let powerRankKeys = Object.keys(powerRanks);
+let colorMap = {
+    white: { progress: '#eee', knob: '#ddd' },
+    green: { progress: '#24C82F', knob: '#1BE028' },
+    yellow: { progress: '#DCE54A', knob: '#F3FF25' },
+    red: { progress: '#CC2626', knob: '#FF2222' },
+};
 
 export default {
     data() {
@@ -47,6 +61,10 @@ export default {
                 nobRadius: 2,
                 side: 250 // pretend its diameter
             },
+
+            circleColor: '#777',
+            progressColor: '#eee',
+            knobColor: '#ddd'
         };
     },
     computed: {
@@ -59,11 +77,73 @@ export default {
         powerRankMid() {
             return this.currentRank.mid;
         },
+        showMid() {
+            if (!this.powerRankKey) return false;
+            return (this.powerRankKey.search(/c\d00/) < 0);
+        },
+        color() {
+            let rank = this.currentRank; // object
+            let result = this.rollResult; // number
+            // where is result in rank obj colors?
+            let showColor;
+            ['white', 'green', 'yellow', 'red'].forEach(color => {
+                if (Array.isArray(rank[color])) {
+                    // is it between the two numbers?
+                    if (rank[color][0] <= result || result >= rank[color][1]) {
+                        showColor = color;
+                    }
+                } else {
+                    // is it the same?
+                    if (result === rank[color]) {
+                        showColor = color;
+                    }
+                }
+            });
+            return colorMap[showColor] || colorMap.white;
+        },
     },
 };
 </script>
 
 <style>
+:root {
+    --text-color: #eee;
+}
+h1 {
+    color: var(--text-color);
+    margin: 0 0 1em;
+}
+main {
+    background-color: #2e3238;
+    box-sizing: border-box;
+    padding-top: 1em;
+    text-align: center;
+}
+circle, path {
+    transition: stroke 0.5s;
+}
+.circle-slider-container {
+    display: inline-block;
+    position: relative;
+    vertical-align: top;
+}
+.circle-slider-label {
+    color: var(--text-color);
+    font-size: 3em;
+    left: 0;
+    line-height: 5;
+    position: absolute;
+    text-align: center;
+    top: 0;
+    width: 100%;
+}
+.circle-slider-graphic {
+    position: relative;
+    z-index: 1;
+}
+.roll-result-label::after {
+    content: '%';
+}
 .power-rank-mid {
     font-size: 0.9em;
 }
